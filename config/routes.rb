e9dadcabb9aac1c942e64bb35d9bec0b16b66610
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2014-01-14 00:08:15 -0500

    Add about, contribute and coming soon pages to the site

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Dudefest::Application.routes.draw do
   resources :movies, :articles
 
+  get 'about', to: 'about#index'
+  get 'contribute', to: 'contribute#index'
+  get 'coming_soon', to: 'coming_soon#index'
+
   mount Ckeditor::Engine => '/ckeditor'
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   devise_for :users
