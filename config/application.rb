53fa0f9a8f58c77ddcf6d99573810686d6224338
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2014-01-15 20:25:33 -0500

    assets with no digest

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Dudefest
     config.assets.version = '1.2'
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w(ckeditor/*)
-    config.assets.precompile += %w(bootstrap-wysihtml5.*)
+    config.assets.digest = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
