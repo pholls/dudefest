207bde9837d354973695d025a52e1b62cb570816
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2014-01-25 17:16:54 -0500

    Add quotes count to users in the schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140123041306) do
+ActiveRecord::Schema.define(version: 20140125200624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -280,6 +280,7 @@ ActiveRecord::Schema.define(version: 20140123041306) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "byline"
+    t.integer  "quotes_count"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
