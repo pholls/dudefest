118792ac4fa9e5ff47ca54aaaba65372f17198a4
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2014-01-26 15:59:06 -0500

    Clean up thing category model, and shorten the maximum length of category

--- app/models/thing_category.rb
+++ app/models/thing_category.rb
@@ -1,18 +1,15 @@
 class ThingCategory < ActiveRecord::Base
   has_many :things, inverse_of: :thing_category
-  validates :category, presence: true, length: { in: 3..32 }, uniqueness: true
+  validates :category, presence: true, length: { in: 3..15 }, uniqueness: true
 
   rails_admin do
     label 'Categories'
     object_label_method :category
     parent Thing
+    include_fields :category
+
     list do
       sort_by :category
-      include_fields :category
-    end
-
-    edit do
-      include_fields :category
     end
   end
 end
