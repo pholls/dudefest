890f288ee32f0e29c4991ac0889284178bb838dd
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2014-02-01 03:06:09 -0500

    Attempt again to get site map to work

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,6 @@
 CarrierWave.configure do |config|
   config.cache_dir = '#{Rails.root}/tmp'
+  config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['S3_KEY'],
