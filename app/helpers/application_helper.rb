00ea6c180d3bb9908301a4856ac843e2db557e1f
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2014-02-09 14:26:20 -0500

    Revert "Try valid image url"
    This reverts commit f56f43244c131f027a7ca0d4833ef25a5b55710c.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,24 +6,4 @@ module ApplicationHelper
   def eastern(datetime)
     datetime.in_time_zone('Eastern Time (US & Canada)')
   end
-
-  def meta_keywords(tags = nil)
-    if tags.present?
-      content_for :meta_keywords, tags
-    elsif content_for?(:meta_keywords) 
-      [content_for(:meta_keywords), APP_CONFIG['meta_keywords']].join(', ') 
-    else 
-      APP_CONFIG['meta_keywords']
-    end
-  end
-
-  def meta_description(desc = nil)
-    if desc.present?
-      content_for :meta_description, desc
-    elsif content_for?(:meta_description) 
-      content_for(:meta_description) 
-    else
-      APP_CONFIG['meta_description']
-    end
-  end
 end
