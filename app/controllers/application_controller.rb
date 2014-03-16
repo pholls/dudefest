251e620d181a655df72c4d70b8ae8dc84ac73d11
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2014-03-16 03:22:36 -0400

    Let all users have bio pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,10 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_in) { |u| 
         u.permit(:username, :password, :remember_me) 
       }
+      devise_parameter_sanitizer.for(:account_update) { |u|
+        u.permit(:username, :email, :name, :password, :password_confirmation,
+                 :current_password, :bio)
+      }
     end
 
   private
