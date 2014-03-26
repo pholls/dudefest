6b9822f3c86666406a473c95e171fbae1f97e9dd
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2014-03-26 00:37:41 -0400

    Only set fog things if fog is in use

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,15 @@
 CarrierWave.configure do |config|
   config.cache_dir = '#{Rails.root}/tmp'
-  Rails.env.production? ? config.storage = :fog : config.storage = :file
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['S3_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET']
-  }
-  config.fog_directory = ENV['S3_BUCKET']
+  if Rails.env.production? 
+    config.storage = :fog
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET']
+    }
+    config.fog_directory = ENV['S3_BUCKET']
+  else
+    config.storage = :file
+  end
+  
 end
