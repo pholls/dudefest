a80f3afd894a8282da4f9e4b48eec12f9774e648
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2014-05-25 19:03:00 -0400

    Column index sidebar

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -1,6 +1,7 @@
 class ColumnsController < ApplicationController
   def index
     # columns are set in application controller
+    @articles = Article.public.first(10)
     @meta_description = 'The Dudefest.com dudes write articles that follow '\
                         'one of a few specific formats. These are those '\
                         'formats'
