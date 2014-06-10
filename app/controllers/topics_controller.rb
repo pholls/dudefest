aed490bfd7e52f51fb36a89adf7a3c460ea9b201
Joe Kennedy <Joseph.Stephen.Kennedy@gmail.com>
2014-06-10 01:44:54 -0400

    Sidebar articles on topics show page should be the most recently posted articles

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,8 @@
 class TopicsController < ApplicationController
   def show
-    @topic = Topic.find_by topic: params[:id].sub('+', ' ')
+    @topic = Topic.find_by topic: params[:id].gsub('+', ' ')
     @articles = Article.public_articles.select { |a| a.topic == @topic }
+    @all_articles = Article.public.first(10)
     @header = 'ON THE SUBJECT OF ' + @topic.topic.upcase
   end
 end
